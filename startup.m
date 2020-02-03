77eb644e27f658b8688af502d861b64dc08a618d
demagalha <demagalha@hotmail.com>
2020-02-03 14:44:12 -0300

    fixed path

--- startup.m
+++ startup.m
@@ -2,12 +2,14 @@ a = mfilename('fullpath');
 IGA = strrep(a,'startup','IGA');
 Examples = strrep(a,'startup','Examples');
 NURBS = strrep(a,'startup','NURBS');
+Models = strrep(a,'startup','Models')
 
 addpath (IGA)
 addpath (Examples)
 addpath (NURBS)
+addpath (Models)
 
-clear a IGA Examples NURBS
+clear a IGA Examples NURBS Models
 
 %ideally we would want to add to the matlab search path \MATLAB\...\bin a
 %simple startup file or add the whole package inside the searchpath, this
