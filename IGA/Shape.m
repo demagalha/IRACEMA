4da1c1fbd057c9531041555154a41914a7cdb28b
unknown <dshguilherme@gmail.com>
2020-03-06 10:44:12 -0300

    Gave support to multidimensional models. Testing needed.

--- IGA/Shape.m
+++ IGA/Shape.m
@@ -1,4 +1,4 @@
-function [ddR, Jacobian] = Shape(pu,u,U,P,MAX_ORDER_OF_DERIVATIVES)
+function [ddR, J] = Shape(Model,u,MAX_ORDER_OF_DERIVATIVES)
 %% INPUTS
 % pu - The spline's degree
 %  u - The point in [0,1] domain where the Shape funs are to be evaluated
@@ -15,25 +15,52 @@ function [ddR, Jacobian] = Shape(pu,u,U,P,MAX_ORDER_OF_DERIVATIVES)
 % J - Jacobian
 
 %% Algorithm
-s = FindSpanLinear(length(U)-pu-2,pu,u,U); % Span in which lies u
-Basis = DersBasisFun(s,u,pu,MAX_ORDER_OF_DERIVATIVES,U);
-
-% Shape function, first derivatives and Jacobian
-
-ACP = P(s-pu+1:s+1,:); % Active Control Points
+switch Model.type
+    case 'curve'
+        p = Model.pu;
+        U = {Model.U};
+    case 'surf'
+        p = [Model.pu; Model.pv];
+        U = {Model.U; Model.V};
+    case 'volume'
+        p = [Model.pu; Model.pv; Model.pw];
+        U = {Model.U; Model.V; Model.W};
+end
+for i=1:numel(p)
+    s(i) = FindSpanLinear(length(U{i})-p(i)-2,p(i),u(i),U{i});
+    Basis{i} = DersBasisFun(s(i),u(i),p(i),MAX_ORDER_OF_DERIVATIVES,U{i});
+end
+P = Model.get_point_cell;
+switch Model.type
+    case 'curve'
+        ACP = P(s(1)-p(1)+1:s(1)+1);
+    case 'surf'
+        ACP = P(s(1)-p(1)+1:s(1)+1,s(2)-p(2)+1:s(2)+1);
+    case 'volume'
+        ACP = P(s(1)-p(1)+1:s(1)+1,s(2)-p(2)+1:s(2)+1,s(3)-p(3)+1:s(3)+1);
+end
+ACP = reshape(ACP,numel(ACP),1);
+ACP = cell2mat(ACP); % Active Control Points
 Weights = ACP(:,4); % Weights of the Active Control Points
 PP = ACP(:,1:3); 
 
 clear ACP
-
-W = Basis(1,:)*Weights; % Sum of Basis*Weights
-R = Basis(1,:)/W; % Rational Basis Funs
-x = Basis(1,:)*(PP.*Weights); % x y z coordinates
-
-Jacobian = Basis(2,:)*(PP.*Weights);
+for i=1:MAX_ORDER_OF_DERIVATIVES+1
+    access = length(u);
+    b = Basis{access}(i,:);
+    while access > 1
+        b = kron(Basis{access-1}(i,:),b); % Kronecker Product Rules! :D
+        access = access-1;
+    end
+    B(i,:) = b;
+end
+W = B(1,:)*Weights; % Sum of Basis*Weights
+R = B(1,:)/W; % Rational Basis Funs
+x = B(1,:)*(PP.*Weights); % x y z coordinates
+Jacobian = B(2,:)*(PP.*Weights);
 J = norm(Jacobian);
-dW = Basis(2,:)*Weights; % Sum of dN*Weights
-dRdu = (1/(W^2))*(W*Basis(2,:) -dW*Basis(1,:));
+dW = B(2,:)*Weights; % Sum of dN*Weights
+dRdu = (1/(W^2))*(W*B(2,:) -dW*B(1,:));
 dR = dRdu/J;
 ddR = cell(MAX_ORDER_OF_DERIVATIVES,1);
 ddR{1} = R;
@@ -45,14 +72,14 @@ if MAX_ORDER_OF_DERIVATIVES > 1
     count = 1;
     while count < MAX_ORDER_OF_DERIVATIVES
         count = count +1;
-        A = Weights'.*Basis(count+1,:);
-        Wk = Basis(count+1,:)*Weights;
+        A = Weights'.*B(count+1,:);
+        Wk = B(count+1,:)*Weights;
         WW(count+1) = Wk;
-        sum = 0;
+        soma = 0;
         for j=1:count
-            sum = sum +nchoosek(count,j)*WW(j)*ddR{count-j+1};
+            soma = soma +nchoosek(count,j)*WW(j)*ddR{count-j+1};
         end
-        ddR{count+1} = (A - sum)/(W*J^count);
+        ddR{count+1} = (A - soma)/(W*J^count);
     end
 end
 end
