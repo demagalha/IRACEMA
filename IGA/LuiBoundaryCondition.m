1d45dbf46d76d4c2fcd3e97660ce1cfd59f6cfb5
Guilherme H. da Silva <dshguilherme@gmail.com>
2020-06-16 10:14:47 -0300

    Diffusion Example and UserGuide
    Finished the Diffusion Example, now working to convergence. Also added a userguide using the Live Editor scripts.

--- IGA/LuiBoundaryCondition.m
+++ IGA/LuiBoundaryCondition.m
@@ -1,100 +1,101 @@
-function [K,M] =  LuiBoundaryCondition(K,M,Boundary,direction,border,Model1,Model2,InverseFunction)
-[INN, IEN, nel, nen] = Model1.get_connectivity;
-
+function K =  LuiBoundaryCondition(Omega1,Omega2,K,BoundaryData,InverseFunction,Mode)
+[INN, IEN, nel, nen] = Omega1.get_connectivity;
 ID = reshape(1:max(max(IEN)),1,max(max(IEN)));
 LM = zeros(nen,nel);
     for i=1:nel
         LM(:,i) = reshape(ID(:,IEN(:,i)),nen,1);
     end
-pu = Model1.pu;
-pv = Model1.pv;
-U = Model1.U;
-V = Model1.V;
-P = Model1.get_point_cell;
+pu = Omega1.pu;
+pv = Omega1.pv;
+U = Omega1.U;
+V = Omega1.V;
+p = [pu; pv];
+Knots = {U,V};
+% P = Omega1.get_point_cell;
  % Get Gauss-Legendre Quadrature Points
     [u, wu] = getGP(pu);
     [v, wv] = getGP(pv);
+    quadpoints = {u, wu; v, wv};
     N_QUAD_U = length(u);
     N_QUAD_V = length(v);
-    N_ELE_DOF = nen;
-
-% Find the elements of the Boundary
-elements = [];
-for i=1:size(IEN,2)
-    a = intersect(IEN(:,i),Boundary);
-    if isempty(a)
-        continue
-    else
-        elements = [elements; i];
-    end
-end
-if strcmp(direction,'u')
-    N_QUAD = N_QUAD_U;
-    v = ones(N_QUAD,1);
-    wv = v;
-    if border == 0
-        v = -v;
-    end
-elseif strcmp(direction,'v')
-    N_QUAD = N_QUAD_V;
-    u = ones(N_QUAD,1);
-    wu = u;
-    if border == 0
-        u = -u;
-    end
-else
-    error('Invalid direction. Please choose string u or string v as input.')
-end
-
+    N_QUAD = [N_QUAD_U; N_QUAD_V];
+N_ELE_DOF = nen;
+% Get the elements of the Boundary
+elements = BoundaryData(:,1);
 for ee=1:numel(elements)
     e = elements(ee);
     K_e = zeros(N_ELE_DOF);
     M_e = K_e;
-    nu = INN(IEN(1,e),1);
-    nv = INN(IEN(1,e),2);
-    for i=N_QUAD
-        [R, dR, J] = Shape2D(Model1,u(i),v(i),e,P,IEN,INN);
-        Jmod = abs(J*wu(i)*wv(i));
-        %% Robin Boundary Condition
-        % We have to find the g_l and f_l to add to our matrices
-        % This is done by accessing the virtual boundary on Model2
-        % There are several ways to estimate the derivative in Omega2,
-        % And we will do a finite difference estimation.
-        uu = ((U(nu+1)-U(nu))*u(i) +U(nu+1)+U(nu))/2; 
-        vv = ((V(nv+1)-V(nv))*v(i) +V(nv+1)+V(nv))/2;
-        x = Model1.eval_point(uu,vv).x;
-        y = Model1.eval_point(uu,vv).y;
+    ni = INN(IEN(1,e),:);
+    parametric_direction = BoundaryData(ee,2); % Direction of the Boundary
+    direction = setdiff([1,2],parametric_direction); % Direction of Integration
+    pidx = ni(parametric_direction)-p(parametric_direction):ni(parametric_direction);
+    q = quadpoints{direction,1};
+    w = quadpoints{direction,2};
+    Domain = Knots{direction};
+    n = ni(direction);
+    h = Domain(n+1) - Domain(n);
+    if h < sqrt(eps)
+        continue
+    end
+    bound_val = BoundaryData(ee,3);
+    robin = BoundaryData(ee,4);
+    K_e = zeros(prod(p+1));
+    for i=N_QUAD(direction)
+        if parametric_direction == 2
+            uu = ((U(n+1)-U(n))*u(i) +U(n+1)+U(n))/2; 
+            vv = bound_val;
+        elseif parametric_direction == 1
+            vv = ((V(n+1)-V(n))*v(i) +V(n+1)+V(n))/2;
+            uu = bound_val;
+        end
+        x = Omega1.eval_point(uu,vv).x;
+        y = Omega1.eval_point(uu,vv).y;
         u2 = InverseFunction{1}(x);
         v2 = InverseFunction{2}(y);
-        perturbation = sqrt(eps);
-        if border == 1
-            perturbation = -perturbation;
+        switch Mode
+            case 'finite'
+                if direction == 1
+                    delta = [sqrt(eps)*u2, 0];
+                elseif direction == 2
+                    delta = [0, sqrt(eps)*v2];
+                end
+                dx0 = Omega2.eval_point(u2,v2);
+                dx1 = Omega2.eval_point(u2+delta(1),v2+delta(2));
+                dzdx = (dx1.z - dx0.z)/(dx1.x - dx0.x);
+                dzdy = (dx1.z - dx0.z)/(dx1.y - dx0.y);
+
+                % Calculate the normal vector
+                BoundaryDirection = [dx1.x-dx0.x; dx1.y-dx0.y];
+                normal = [0 -1; 1 0]*(BoundaryDirection/norm(BoundaryDirection));
+                Omega2_derivatives = [dzdx, dzdy];
+                dz2 = dot(Omega2_derivatives,normal);
+                z2 = Omega2.eval_point(u2,v2).z;
+                % Computing the "lifting" functions:
+                g = z2;
+                f = dz2;
+                BETA = -f/g;
+                if isnan(BETA)
+                    BETA = 0;
+                end
+            case 'cont'
+                P2 = Omega2.get_point_cell;
+                [INN2, IEN2, ~, ~] = Omega2.get_connectivity;
+                qu2 = (2*u2 +U2(nu) +U2(nu+1))/(U2(nu+1)-U(nu));
+                qv2 = (2*v2 +V2(nv) +V2(nv+1))/(V2(nv+1)-V(nv));
+                [R2, dR2, J2] = Shape2D(Omega,qu2,qv2,e2,P2,IEN2,INN2);
         end
-        
-        dx0 = Model2.eval_point(u2,v2);
-        dx1 = Model2.eval_point(u2,v2+perturbation);
-        dzdx = (dx1.z - dx0.z)/(dx1.x - dx0.x);
-        
-        dy0 = Model2.eval_point(u2,v2);
-        dy1 = Model2.eval_point(u2+perturbation,v2);
-        dzdy = (dy1.z - dy0.z)/(dy1.y - dy0.y);
-        if strcmp(direction,'u')
-            dz2 = dzdy;
-        elseif strcmp(direction,'v')
-            dz2 = dzdx;
-        end
-        
-        z2 = Model2.eval_point(u2,v2).z;
-        
-        % Computing the "lifting" functions:
-        g = z2;
-        f = -abs(dz2);
-%         K_e = K_e + Jmod*((g)*(dR(:,1)*dR(:,1)' +dR(:,2)*dR(:,2)') -f*(R*R'));
-          K_e = K_e -60*Jmod*(f/g)*(R*R');
-%         M_e = M_e + Jmod*f/g*(R*R');
+                
+                
+        qu = q(i);
+
+        Boundary = [parametric_direction, direction, bound_val];
+        [R, ~, J] = BoundaryShape(Omega1,qu,e,IEN,INN,Boundary);
+%         F_e = F_e + abs(J)*w(i)*R*robin;
+        K_e = K_e + abs(J)*w(i)*R*BETA*R';
+        %% Robin Boundary Condition
     end
     idx = LM(:,e)';
     K(idx,idx) = K(idx,idx) + K_e;
-    M(idx,idx) = M(idx,idx) + M_e;
 end
 end
