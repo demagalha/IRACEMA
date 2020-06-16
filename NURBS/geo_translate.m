eec83fb6c1de4839c7be56d16b8b2b441049c247
demagalha <demagalha@hotmail.com>
2020-06-16 14:26:41 -0300

    add options

--- NURBS/geo_translate.m
+++ NURBS/geo_translate.m
@@ -4,6 +4,18 @@ P = Model.get_point_cell;
 tam = size(P);
 vec = [vector 0];
 switch Model.type
+	case 'curve'
+		for i=1:Model.nu+1
+			P{i} = P{i} + vec;
+			translate = Geometry('curve',Model.pu,Model.U,P);
+		end
+	case 'surf'
+		for i=:tam(1)
+			for j=1:tam(2)
+				P{i,j} = P{i,j} + vec;
+			end
+		end
+		translate = Geometry('surf',Model.pu,Model.U,Model.pv,Model.V,P);
     case 'volume'
         for i=1:tam(1)
             for j=1:tam(2)
